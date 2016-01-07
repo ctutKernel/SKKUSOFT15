ad530fb4d0422cd9df8477a50f02ab77af28ed38
ctutKernel <pjh3974@gmail.com>
2016-01-07 19:10:50 +0900

    makeDir.py_Ch

--- dirMaker/makeDir.py
+++ dirMaker/makeDir.py
@@ -4,7 +4,7 @@ args = sys.argv[1:]
 dirName="clr_"
 subDirName=["c","java","py"]
 os.system("mkdir "+dirName+args[0]+"_"+args[1]+"_"+args[2])
-os.chdir(os.getcwd()+"/"+dirName+args[0]+"_"+args[1])
+os.chdir(os.getcwd()+"/"+dirName+args[0]+"_"+args[1]+"_"+args[2])
 os.system("touch "+"Question.md");
 for name in subDirName:
 	os.system("mkdir "+name)
